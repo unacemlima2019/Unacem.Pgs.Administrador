faf80ece5ff944133599068558e0bb4152833814
Jorge Illescas <jorgeluis_illescas@hotmail.com>
2021-08-12 17:34:24 -0500

    Actualiza consulta locales

--- Unacem.Pgs.Cpra.Infraestructura.Datos/Consultas/ConsultarProgresol.cs
+++ Unacem.Pgs.Cpra.Infraestructura.Datos/Consultas/ConsultarProgresol.cs
@@ -44,54 +44,48 @@ namespace Unacem.Pgs.Admin.Infraestructura.Datos.Consultas
             using (var conexion = new SqlConnection(_cadenaConexion))
             {
                 var resultado = await conexion.QueryAsync<ModeloVistaProgresol>(
-                                            @"SELECT
-                                                  SA.DSC_RUC_CLIENTE AS RucCliente,
-                                                  SA.COD_PDV AS CodPdv,
-                                                  SA.DSC_NOMBRE_COMERCIAL as NombreComercial,
-                                                  PG.DSC_CELULAR AS Celular,
-                                                  SA.DSC_DIRECCION AS Direccion,
-                                                  SA.DSC_LONGITUD AS Latitud,
-                                                  SA.DSC_LATITUD AS Longitud,
-                                                  SA.COD_ZONA AS CodZona,
-                                                  SA.DSC_ZONA AS Zona,
-                                                  SA.COD_TERRITORIO AS CodTerritorio,
-                                                  SA.DSC_TERRITORIO AS Territorio,
-                                                  SA.COD_SUB_TERRITORIO AS CodSubTerritorio,
-                                                  SA.DSC_SUB_TERRITORIO AS SubTerritorio,
-                                                  US.COD_USUARIO AS Usuario,
-                                                  SA.DSC_FLAG_TIENDA AS FlagTienda,
-												  SA.DSC_TIPO_PROGRESOL AS TipoProgresol,
-                                                  case  
-                                                       when SA.DSC_FLAG_TIENDA <> 'X' then 'No Tienda'      
-                                                       when pg.COD_TIENDA_PROGRESOL is null AND SA.DSC_FLAG_TIENDA = 'X' then 'Pendiente'          
-                                                  else 'En Linea'
-                                                  end as Accion,
-                                                  pg.COD_TIENDA_PROGRESOL AS CodTiendaProgresol
-                                                FROM [PGS].[dbo].[PGSTB_TEMP_FUN_CLIENTESPS_SAP] SA
-                                                  LEFT join [dbo].[PGSTB_TIENDA_PROGRESOL] PG
-                                                on sa.COD_PDV=PG.DSC_COD_LOCAL_SAP
-                                                  LEFT join  [dbo].[SEGTB_USUARIO] US
-                                                on sa.DSC_RUC_CLIENTE=US.NUM_DOCUMENTO
+                                            @"SELECT	SA.DSC_RUC_CLIENTE			AS RucCliente,
+		                                                SA.COD_PDV					AS CodPdv,
+		                                                SA.DSC_NOMBRE_COMERCIAL		as NombreComercial,
+		                                                PG.DSC_CELULAR				AS Celular,
+		                                                SA.DSC_DIRECCION			AS Direccion,
+		                                                SA.DSC_LONGITUD				AS Latitud,
+		                                                SA.DSC_LATITUD				AS Longitud,
+		                                                SA.COD_ZONA					AS CodZona,
+		                                                SA.DSC_ZONA					AS Zona,
+		                                                SA.COD_TERRITORIO			AS CodTerritorio,
+		                                                SA.DSC_TERRITORIO			AS Territorio,
+		                                                SA.COD_SUB_TERRITORIO		AS CodSubTerritorio,
+		                                                SA.DSC_SUB_TERRITORIO		AS SubTerritorio,
+		                                                ''							AS Usuario,
+		                                                SA.DSC_FLAG_TIENDA			AS FlagTienda,
+		                                                SA.DSC_TIPO_PROGRESOL		AS TipoProgresol,
+                                                        case  
+                                                            when SA.DSC_FLAG_TIENDA <> 'X' then 'No Tienda'      
+                                                            when pg.COD_TIENDA_PROGRESOL is null AND SA.DSC_FLAG_TIENDA = 'X' then 'Pendiente'          
+                                                        else 'En Linea'
+                                                        end							as Accion,
+                                                        pg.COD_TIENDA_PROGRESOL		AS CodTiendaProgresol
+                                                FROM	[PGS].[dbo].[PGSTB_TEMP_FUN_CLIENTESPS_SAP] (nolock) SA
+		                                                LEFT join [dbo].[PGSTB_TIENDA_PROGRESOL]	(nolock) PG	on sa.COD_PDV	=	PG.DSC_COD_LOCAL_SAP
                                                 group by SA.DSC_RUC_CLIENTE,
-                                                   SA.COD_PDV,
-                                                  SA.DSC_DIRECCION,
-                                                  SA.DSC_NOMBRE_COMERCIAL,
-                                                  SA.COD_ZONA,
-                                                  SA.DSC_ZONA,
-                                                  SA.COD_TERRITORIO,
-                                                  SA.DSC_TERRITORIO,
-                                                  SA.COD_SUB_TERRITORIO,
-                                                  SA.DSC_SUB_TERRITORIO,
-                                                  US.COD_USUARIO,
-                                                  pg.COD_TIENDA_PROGRESOL,
-                                                  pg.COD_TIENDA_PROGRESOL,
-                                                  PG.DSC_CELULAR,
-                                                  SA.DSC_FLAG_TIENDA,
-                                                  SA.DSC_LONGITUD,
-                                                  SA.DSC_LATITUD,
-												  SA.DSC_TIPO_PROGRESOL
-                                                 order by codpdv
-                                            ");
+		                                                SA.COD_PDV,
+		                                                SA.DSC_DIRECCION,
+		                                                SA.DSC_NOMBRE_COMERCIAL,
+		                                                SA.COD_ZONA,
+		                                                SA.DSC_ZONA,
+		                                                SA.COD_TERRITORIO,
+		                                                SA.DSC_TERRITORIO,
+		                                                SA.COD_SUB_TERRITORIO,
+		                                                SA.DSC_SUB_TERRITORIO,
+		                                                pg.COD_TIENDA_PROGRESOL,
+		                                                pg.COD_TIENDA_PROGRESOL,
+		                                                PG.DSC_CELULAR,
+		                                                SA.DSC_FLAG_TIENDA,
+		                                                SA.DSC_LONGITUD,
+		                                                SA.DSC_LATITUD,
+		                                                SA.DSC_TIPO_PROGRESOL
+                                                order by codpdv");
 
                 return new ModeloVista<ModeloVistaProgresol>(
                     new ResultadoConsulta
